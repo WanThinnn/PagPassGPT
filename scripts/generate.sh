ac788152481027b050d0e3046ac44bf84d34f39f
wanthinnn <thienlai159@gmail.com>
2025-05-10 02:15:15 +0700

    edit 'python' -> 'python3.8' ./scripts/*

--- scripts/generate.sh
+++ scripts/generate.sh
@@ -6,10 +6,10 @@ model_path="./model/last-step/"  # Đường dẫn đến mô hình đã huấn 
 output_path="./generate/"  # Thư mục đầu ra cho mật khẩu sinh ra
 
 # 1. Tính toán tỷ lệ pattern (cần cho DC-GEN)
-python get_pattern_rate.py --dataset_path=$cleaned_dataset
+python3.8 get_pattern_rate.py --dataset_path=$cleaned_dataset
 
 # 2. Sinh mật khẩu sử dụng DC-GEN (phương pháp nâng cao)
-# python DC-GEN.py --model_path=$model_path --output_path=$output_path --generate_num=1000000 --batch_size=5000 --gpu_num=2 --gpu_index=4
+# python3.8 DC-GEN.py --model_path=$model_path --output_path=$output_path --generate_num=1000000 --batch_size=5000 --gpu_num=2 --gpu_index=4
 
 # 3. Hoặc sinh mật khẩu không sử dụng DC-GEN (phương pháp thông thường)
 #python normal-gen.py --model_path=$model_path --output_path=$output_path --generate_num=1000000 --batch_size=5000 --gpu_num=2 --gpu_index=4
